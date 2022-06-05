46655220cfe19f914858d68b5073380faae57fff
Andrew Taylor <rolyata47@gmail.com>
2022-06-05 16:03:00 -0500

    Optimizing

--- imagemagic.py
+++ imagemagic.py
@@ -27,27 +27,43 @@ def get_ij(x, y, n):
     # j = n/2 - y
     return [int(x + n/2), int(n/2 - y)]
 
-# Rotates a vector 90 degrees counterclockwise around the origin
-def rot_vec_90(x, y):
-    A = [[0, -1], 
-         [1, 0]]
-    return np.dot(A, [x, y]).tolist()
-
 # Rotates an image 90 degrees counterclockwise around the origin
 def rot_img_90(filename):
+    # Read the image from file
     img = mpimg.imread(filename)
     n, m = len(img), len(img[0])
     print('%dx%d matrix (%s)' %(n, m, filename))
 
+    # Make a blank white canvas to contain the image
     canvas = make_canvas(img)
     N = len(canvas)
     print('%dx%d canvas' %(N, N))
 
+    # Make a blank white canvas for the new image
+    # We are using the letter 't' as a prefix or suffix to mean "transformed" (by a linear transformation)
     tcanvas = [[[255, 255, 255] for i in range(N)] for j in range(N)]
+
+    # Create a matrix X for the (x, y) coordinates of image pixels
+    X = []
     for row in range(int(n/2), int(N-n/2)):
         for col in range(int(m/2), int(N-m/2)):
-            [x, y] = get_xy(col, row, N)
-            [x, y] = rot_vec_90(x, y)
-            [i, j] = get_ij(x, y, N)
-            tcanvas[j][i] = canvas[row][col]
+            vertex = get_xy(col, row, N)
+            X.append(np.array(vertex))
+    X = tuple(X)
+    X = np.column_stack(X)  
+
+    # Multiply X by a rotation matrix and store in a new matrix Y
+    A = [[0, -1], 
+         [1, 0]]
+    Y = np.dot(A, X)
+
+    # Write the new image (after transformation) to its canvas
+    num_vertices = len(X[0])
+    for i in range(num_vertices):
+        x, y = X[0][i], X[1][i]
+        xt, yt = Y[0][i], Y[1][i]
+        [i, j] = get_ij(x, y, N)
+        [it, jt] = get_ij(xt, yt, N)
+        tcanvas[jt][it] = canvas[j][i]
+
     return tcanvas
