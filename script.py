9b0384b551356f9aff0f0ab39e6ac5a81ccefcd9
Andrew Taylor <rolyata47@gmail.com>
2022-06-06 09:52:47 -0500

    Adding transform, reflecty and reflectx methods

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import matplotlib.image as mpimg
 import math
 
 FILENAME = 'stacksofwheat.jpg'
+
 # img = mpimg.imread(FILENAME)
 # img = imagemagic.rotate(FILENAME, 2*math.pi)
 img = imagemagic.reflectx(FILENAME)
